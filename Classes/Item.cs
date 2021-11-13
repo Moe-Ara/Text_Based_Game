0c31fdca4c54c50d2425428af8893c0cc7e807c4
Moe-Ara <hulk78901@gmail.com>
2021-11-13 05:09:52 +0100

    made it a CS project

--- Classes/Item.cs
+++ Classes/Item.cs
@@ -1,11 +1,15 @@
 using System;
 class item{
     private String itemName;
+    private String colour;
     private String description;
+    private bool isPickable;
 
-    public item(String itemName, String description =""){
+    public item(String itemName,String colour="", String description ="", bool isPickable=false){
         this.itemName=itemName;
         this.description= description;
+        this.isPickable=isPickable;
+        this.colour=colour;
     }
     String getItemName(){
         return itemName;
@@ -13,8 +17,17 @@ class item{
     String getDescription(){
         return description;
     }
+    String getColour(){
+        return colour;
+    }
+    bool getIsPickable(){
+        return isPickable;
+    }
     String setDescription(String description){
         this.description=description;
+    }
+    String setColour(String colour){
+        this.colour =colour;
     }
     bool hasDescription(){
         return (getDescription!="");
---
+++ Classes/Player.cs
@@ -0,0 +1,11 @@
+using System;
+using item.cs;
+using Places.cs;
+class Player{
+    private String name;
+    private String sex;
+    private String Description;
+    protected HashSet<item> inventory;
+
+
+}
