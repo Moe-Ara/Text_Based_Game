e854918ce96878949b7dd86c9acc9bee28c26c6b
Moe-Ara <hulk78901@gmail.com>
2021-11-14 01:46:39 +0100

    Create a Map in This calss to Use it in the game

---
+++ PrimaryService/Buidling_the_game/Map.cs
@@ -0,0 +1,26 @@
+using System;
+namespace TextBasedGame
+{
+    public class Map{
+        protected Place Start;
+        protected Place FrontLawn;
+        protected Place House;
+        protected Place Basement; 
+        protected Place Shed;
+        protected Place Demonic_room;
+        protected Place Kitchen;
+        protected Place BackLawn;
+        protected Place Field;
+        protected Place SecondFloor;
+        protected Place Attic;
+        protected Place ParentsRoom;
+        protected Place ChildrensRoom;
+        protected Place Tunnel;
+
+        public Map(){
+            
+        }
+
+    }
+    
+}
