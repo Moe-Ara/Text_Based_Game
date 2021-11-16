25bdfefd37d742f67319a9051799704b39cc8792
Moe-Ara <hulk78901@gmail.com>
2021-11-16 01:15:32 +0100

    Edited Path

--- PrimaryService/Classes/Path.cs
+++ PrimaryService/Classes/Path.cs
@@ -1,6 +1,10 @@
 using System;
 namespace TextBasedGame
 {
+    /*
+    * This is a singly-linked List
+    * It is the path that the player takes, this is will make it easier to go back 
+    */
     internal class Node
     {
         internal Node next;
@@ -21,34 +25,48 @@ namespace TextBasedGame
 
         }
 
-        public void AddAtLast(Place data){
+        public void AddAtLast(Place data)
+        {
             Node newNode = new Node();
-            newNode.Data= data;
-            current.next= newNode;
-            current= newNode;
+            newNode.Data = data;
+            current.next = newNode;
+            current = newNode;
             index++;
         }
-        public void PrintAll(){
+        public void PrintAll()
+        {
             Console.Write("Head ->");
-            Node curr =head;
-            while(curr.next!=null){
-                curr=curr.next;
+            Node curr = head;
+            while (curr.next != null)
+            {
+                curr = curr.next;
                 Console.Write(curr.Data);
                 Console.Write("->");
             }
             Console.Write("NULL");
         }
-        public void RemoveLast(){
-            Node curr=head;
-            while(curr.next.next!=null){
-                curr=curr.next;
+        public void RemoveLast()
+        {
+            Node curr = head;
+            //traverse until you find the last node
+            while (curr.next.next != null)
+            {
+                curr = curr.next;
             }
-            curr.next=null;
+            //if they are at the start
+            if (curr.next == null)
+            {
+                Console.WriteLine("You can't go back any further");
+                return;
+            }
+            curr.next = null;
         }
-        public Place getLast(){
-            Node curr=head;
-            while(curr.next!=null){
-                curr= curr.next;
+        public Place getLast()
+        {
+            Node curr = head;
+            while (curr.next != null)
+            {
+                curr = curr.next;
             }
             return curr.Data;
         }
