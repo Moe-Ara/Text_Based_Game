f5ce25fb9da5bcc2ca794f272980091545ba01cc
Moe-Ara <hulk78901@gmail.com>
2021-11-18 00:19:02 +0100

    Inventory is now in a seperate class

---
+++ PrimaryService/Classes/Inventory.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace TextBasedGame
+{
+    public class Inventory
+    {
+        private Place plyersPlace;
+        protected HashSet<Item> inventory; //player's inventory
+        public Inventory()
+        {
+            inventory = new HashSet<Item>();
+            this.inventory.Add(new Item("Pocket Knife", "", "It is small pocket knife with a wooden grip" +
+            ".\nThe blade seems a bit rusty and dull but it is sharp enough to cut through some thin materials", true));
+        }
+        public String printInventory() //print every item in the inventory
+        {
+            String items = "";
+            foreach (Item x in inventory)
+            {
+                items += x.getItemName() + "; ";
+                //removes the last semicolon
+                items.Remove(items.Length - 1);
+            }
+            return items;
+        }
+        public void addItem(Item item) //add item to inventory
+        {
+            inventory.Add(item);//pick it up
+        }
+        public void removeItem(Item item) //remove item from inventory
+        {
+            inventory.Remove(item); //drop it
+        }
+
+        public bool itemIsInInvenory(Item item)
+        {
+            return inventory.Contains(item);
+        }
+    }
+}
