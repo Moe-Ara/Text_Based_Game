dd2278cbe5e34ffa652ee23b5e77e733cbb60862
Moe-Ara <hulk78901@gmail.com>
2021-12-05 23:47:40 +0100

    basic functionality of the game

--- PrimaryService/Program.cs
+++ PrimaryService/Program.cs
@@ -14,6 +14,18 @@ namespace TextBasedGame
             WelcomingPlayer welcomingPlayer = new WelcomingPlayer();
             Player player = welcomingPlayer._player;
             Map initMap = welcomingPlayer._getIntializedMap;
+            InterpretCommand interpretCommand= new InterpretCommand();
+            GetUserInput getUserInput = new GetUserInput();
+            System.Console.WriteLine("Input your first command here");
+            String str=null;
+            while(true){
+                str=getUserInput.getUserInput().ToLower();
+                if (str.Equals("exit")||str.Equals("quit")||str.Equals("Bye")){
+                    break;
+                }
+            System.Console.WriteLine(interpretCommand.Interpreter(str));
+            }
+            System.Console.WriteLine("Goodbye brave adventurer, may we see eachother in another life");
         }
     }
 }
